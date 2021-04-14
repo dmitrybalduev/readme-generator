9f01696e6bb72c61396d5a691374dfe101d82096
Dmitry <dmitry.balduev@gmail.com>
2021-04-14 17:30:00 -0400

    added a single promt with question

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const inq = require("inquirer");
+const { title } = require("process");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Please enter title"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inq.
+        prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            }
+        ])
+        .then((response) => console.log(response));
+}
 
 // Function call to initialize app
 init();
